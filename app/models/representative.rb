4a232819d0945e24121f2f3b9a7896f74d6c530a
Alysa Liu <alysaluu@berkeley.edu>
2023-11-23 08:54:41 +0000

    pt2 not working

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -3,24 +3,46 @@
 class Representative < ApplicationRecord
   has_many :news_items, dependent: :delete_all
 
+  # rubocop:disable Metrics/BlockLength, Metrics/AbcSize, Metrics/MethodLength
   def self.civic_api_to_representative_params(rep_info)
     reps = []
 
     rep_info.officials.each_with_index do |official, index|
       ocdid_temp = ''
       title_temp = ''
+      street_temp = ''
+      city_temp = ''
+      state_temp = ''
+      zip_temp = ''
+      party_temp = ''
+      photo_url_temp = ''
 
       rep_info.offices.each do |office|
         if office.official_indices.include? index
           title_temp = office.name
           ocdid_temp = office.division_id
+          photo_url_temp = official.photo_url if official.instance_variable_defined?(:@photo_url)
+          unless official.address.nil?
+            street_temp = official.address[0].line1
+            city_temp = official.address[0].city
+            state_temp = official.address[0].state
+            zip_temp = official.address[0].zip
+          end
         end
+        party_temp = official.party
+        print "print this"
+        puts party_temp
       end
-      rep_params = { name: official.name, ocdid: ocdid_temp, title: title_temp }
-      rep = Representative.find_or_create_by(rep_params)
-      reps.push(rep)
-    end
 
+      representative = Representative.find_or_initialize_by(name: official.name, ocdid: ocdid_temp)
+      if representative.new_record?
+        rep_params = { name: official.name, ocdid: ocdid_temp, party: party_temp, photo_url: photo_url_temp, zip: zip_temp, address: street_temp, city: city_temp, state: state_temp }
+        representative = Representative.create!(rep_params)
+        representative.title = title_temp
+        representative.save!
+      end
+      reps.push(representative)
+    end
     reps
   end
 end
