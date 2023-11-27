62cdeda78f16fb028c94e4bee4d785607e0d440d
Winnie Zhu <winniezhu@berkeley.edu>
2023-11-27 05:44:37 +0000

    [fix] rep page bug

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -3,43 +3,34 @@
 class Representative < ApplicationRecord
   has_many :news_items, dependent: :delete_all
 
-  # rubocop:disable Metrics/BlockLength, Metrics/AbcSize, Metrics/MethodLength
   def self.civic_api_to_representative_params(rep_info)
     reps = []
 
     rep_info.officials.each_with_index do |official, index|
-      ocdid_temp = ''
-      title_temp = ''
-      street_temp = ''
-      city_temp = ''
-      state_temp = ''
-      zip_temp = ''
-      party_temp = ''
-      photo_url_temp = ''
+      rep_params = { name: official.name, ocdid: '', party: '', photo_url: '',
+          zip: '', address: '', city: '', state: '' }
+    
 
       rep_info.offices.each do |office|
         if office.official_indices.include? index
-          title_temp = office.name
-          ocdid_temp = office.division_id
-          photo_url_temp = official.photo_url if official.instance_variable_defined?(:@photo_url)
-          unless official.address.nil?
-            street_temp = official.address[0].line1
-            city_temp = official.address[0].city
-            state_temp = official.address[0].state
-            zip_temp = official.address[0].zip
-          end
+          rep_params[:title] = office.name
+          rep_params[:ocdid] = office.division_id
         end
-        #party_temp = official.party
-        party_temp = official.party if official.instance_variable_defined?(:@party)
-        #print "print this"
-        #puts party_temp
+        unless official.address.nil?
+          rep_params[:address] = official.address[0].line1
+          rep_params[:city] = official.address[0].city
+          rep_params[:state] = official.address[0].state
+          rep_params[:zip] = official.address[0].zip
+        end
+
+        rep_params[:photo_url] = official.photo_url if official.instance_variable_defined?(:@photo_url)
+        rep_params[:party] = official.party if official.instance_variable_defined?(:@party)
       end
 
-      representative = Representative.find_or_initialize_by(name: official.name, ocdid: ocdid_temp)
+      representative = Representative.find_or_initialize_by(name: official.name, ocdid: rep_params[:ocdid])
       if representative.new_record?
-        rep_params = { name: official.name, ocdid: ocdid_temp, party: party_temp, photo_url: photo_url_temp, zip: zip_temp, address: street_temp, city: city_temp, state: state_temp }
+
         representative = Representative.create!(rep_params)
-        representative.title = title_temp
         representative.save!
       end
       reps.push(representative)
